b484511ba3fb885c598b0e8d325c7577c42be6c1
Outlaw <155272209+OutlawTwinCoder@users.noreply.github.com>
2025-10-05 10:49:47 -0400

    Add new items to ESX inventory SQL script

--- sql/items_esx.sql
+++ sql/items_esx.sql
@@ -1,6 +1,10 @@
--- Si tu utilises encore la table ESX items (inventaire vanilla), exemple:
 INSERT INTO items (name, label, weight) VALUES
 ('scalpel', 'Scalpel', 1),
+('scalpel_pro', 'Scalpel Pro', 1),
+('surgery_kit', 'Kit chirurgical', 2),
+('cooler', 'Glacière', 5),
+('icepack', 'Pack de glace', 1),
+('gants', 'Gants', 1),
 ('rein', 'Rein', 2),
 ('crane', 'Crâne', 3),
 ('pied', 'Pied', 2),
